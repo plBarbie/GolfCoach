3904b33a47083c8a7e7b3956bf997996aada03d5
PLBarbie <yiyaoguo@bupt.edu.cn>
2024-10-03 16:04:51 +0800

    Done Teaching_Standing

--- android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
+++ android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
@@ -37,7 +37,6 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
     // To define styles and colours for drawing keypoints and bone connections
     private var pointPaint = Paint()
     private var linePaint = Paint()
-    private var textPaint = Paint()
 
     // Adjust the drawn pose coordinates to fit the screen size
     private var scaleFactor: Float = 1f
@@ -95,19 +94,16 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
                         linePaint)
                 }
                 // 使用 PoseEvaluator 评估姿势并显示结果
-//                val feetDistanceEvaluationMessage = poseEvaluator.checkFeetShoulderWidth(poseLandmarkerResult).message
-//                val feetDistanceEvaluationScore = poseEvaluator.checkFeetShoulderWidth(poseLandmarkerResult).score
-//                val armsStraightEvaluationMessage = poseEvaluator.checkArmsStraight(poseLandmarkerResult).message
-//                val armsStraightEvaluationScore = poseEvaluator.checkArmsStraight(poseLandmarkerResult).score
+                val feetDistanceEvaluationMessage = poseEvaluator.checkFeetShoulderWidth(poseLandmarkerResult).message
+                val feetDistanceEvaluationScore = poseEvaluator.checkFeetShoulderWidth(poseLandmarkerResult).score
+                val armsStraightEvaluationMessage = poseEvaluator.checkArmsStraight(poseLandmarkerResult).message
+                val armsStraightEvaluationScore = poseEvaluator.checkArmsStraight(poseLandmarkerResult).score
                 val handsPositionEvaluationMessage = poseEvaluator.checkHandsPosition(poseLandmarkerResult).message
                 val handsPositionEvaluationScore = poseEvaluator.checkHandsPosition(poseLandmarkerResult).score
-//                canvas.drawText(armsStraightEvaluationMessage, 50f, 100f, textPaint)
-//                canvas.drawText(armsStraightEvaluationScore.toString(), 50f, 100f, textPaint)
-
-                // 弹出 Toast 提示，显示评估结果
-//                Toast.makeText(context, feetDistanceEvaluationMessage+feetDistanceEvaluationScore.toString(), Toast.LENGTH_SHORT).show()
-//                Toast.makeText(context, armsStraightEvaluationMessage+armsStraightEvaluationScore.toString(), Toast.LENGTH_SHORT).show()
-                Toast.makeText(context, handsPositionEvaluationMessage+handsPositionEvaluationScore.toString(), Toast.LENGTH_SHORT).show()
+                val scoreTotal = feetDistanceEvaluationScore+armsStraightEvaluationScore+handsPositionEvaluationScore
+                (context as? TeachingActivity)?.showOverlayText(
+                    feetDistanceEvaluationMessage+ "\n" + armsStraightEvaluationMessage +"\n"+ handsPositionEvaluationMessage
+                            +"\n"+ "Your score: " + scoreTotal)
             }
         }
     }
--- android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/TeachingActivity.kt
+++ android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/TeachingActivity.kt
@@ -2,29 +2,30 @@ package com.google.mediapipe.examples.poselandmarker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.mediapipe.examples.poselandmarker.databinding.ActivityMainBinding
 
 class TeachingActivity : AppCompatActivity() {
-    private lateinit var activityMainBinding: ActivityMainBinding
     private val viewModel : MainViewModel by viewModels()
+    private lateinit var statusTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        setContentView(R.layout.activity_teaching)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        activityMainBinding.navigation.setupWithNavController(navController)
-        activityMainBinding.navigation.setOnNavigationItemReselectedListener {
-            // ignore the reselection
-        }
+        statusTextView = findViewById(R.id.realtime_text)
     }
 
+    fun showOverlayText(message: String) {
+        statusTextView.text = message
+        statusTextView.visibility = View.VISIBLE
+    }
     override fun onBackPressed() {
         finish()
     }
